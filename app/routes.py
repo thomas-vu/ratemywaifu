cb3b268a1c684b4d0791f61558d669cbb511c339
Thomas Vu <shuhari@macbook.local>
2019-12-06 17:32:21 -0700

    fixed pagination for index and user pages, but not for waifu pages yet

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
 from app.models import User, Post, Waifu, PendingWaifu, Anime, PendingAnime, Rating, AnimeGenres, WaifuTags, Studio
 from app.email import send_password_reset_email
 from sqlalchemy import or_
-
+from math import ceil
 
 @app.before_request
 def before_request():
@@ -16,48 +16,16 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    #form = PostForm()
-    #if form.validate_on_submit():
-    #    post = Post(body=form.post.data, author=current_user)
-    #    db.session.add(post)
-    #    db.session.commit()
-    #    flash('Your post is now live!')
-    #    return redirect(url_for('index'))
-
-    #page = request.args.get('page', 1, type=int)
-    #posts = current_user.followed_posts().paginate(
-    #    page, app.config['POSTS_PER_PAGE'], False)
-    #next_url = url_for('index', page=posts.next_num) \
-    #    if posts.has_next else None
-    #prev_url = url_for('index', page=posts.prev_num) \
-    #    if posts.has_prev else None
-
-    ratings = Rating.query.all()
-
-    #return render_template('index.html', title='Home', form=form,
-    #                       posts=posts.items, next_url=next_url,
-    #                       prev_url=prev_url, Waifu=Waifu, ratings=ratings)
-
-    return render_template('index.html', title='Home', Waifu=Waifu, ratings=ratings)
-
-
-@app.route('/explore')
-@login_required
-def explore():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('explore', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) \
-        if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
-
+    ratings = Rating.query.order_by(Rating.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=ratings.next_num) if ratings.has_next else None
+    prev_url = url_for('index', page=ratings.prev_num) if ratings.has_prev else None
+    num_ratings = ceil(len(Rating.query.all()) / app.config['POSTS_PER_PAGE'])
+    return render_template('index.html', title='Home', Waifu=Waifu, \
+        ratings=ratings.items, num_ratings=num_ratings, prev_url=prev_url, page=page, next_url=next_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -76,12 +44,10 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -97,7 +63,6 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
@@ -111,7 +76,6 @@ def reset_password_request():
         return redirect(url_for('login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
-
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
@@ -128,30 +92,17 @@ def reset_password(token):
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
 
-
-@app.route('/user/<username>')
+@app.route('/user/<username>', methods=['GET', 'POST'])
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
-
-    ratings = user.ratings.order_by(Rating.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=ratings.next_num) \
-        if ratings.has_next else None
-    prev_url = url_for('user', username=user.username, page=ratings.prev_num) \
-        if ratings.has_prev else None
-
-    return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url, Waifu=Waifu, ratings=ratings.items)
-
+    ratings = user.ratings.order_by(Rating.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=ratings.next_num) if ratings.has_next else None
+    prev_url = url_for('user', username=user.username, page=ratings.prev_num) if ratings.has_prev else None
+    num_ratings = ceil(len(Rating.query.all()) / app.config['POSTS_PER_PAGE'])
+    return render_template('user.html', user=user, Waifu=Waifu,
+        ratings=ratings.items, num_ratings=num_ratings, prev_url=prev_url, page=page, next_url=next_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -166,9 +117,7 @@ def edit_profile():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile',
-                           form=form)
-
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 @app.route('/follow/<username>')
 @login_required
@@ -184,7 +133,6 @@ def follow(username):
     db.session.commit()
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
-
 
 @app.route('/unfollow/<username>')
 @login_required
