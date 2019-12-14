c76169167a570b7fd417e7d04ed8d3584ac1c8f0
Thomas Vu <shuhari@macbook.local>
2019-12-14 13:31:14 -0700

    removed database seeding from Procfile

--- fill_db_with_anime.py
+++ fill_db_with_anime.py
@@ -1,26 +1,26 @@
-from sys import stdin
 from app import db
 from app.models import Anime, Studio, AnimeGenres
 
-A = [line.split('$') for line in stdin]
-
-for show in A:
-    anime = Anime(name         = show[1],
-                  season       = show[3],
-                  year         = show[4],
-                  num_episodes = show[5],
-                  esrb         = show[6],
-                  description  = show[9],
-                  image        = show[2],
-                  url          = show[0],
-                  studio       = show[7])
-    studio = Studio(name       = show[7])
-    B = show[8].split(',')
-    for gn in B:
-        genres = AnimeGenres(anime_name = show[1], genre = gn)
-        db.session.add(genres)
-    #print(anime)
-    db.session.add(anime)
-    db.session.add(studio)
-
-db.session.commit()
+with open('anime.txt') as f:
+    A = [line.split('$') for line in f]
+    
+    for show in A:
+        anime = Anime(name         = show[1],
+                      season       = show[3],
+                      year         = show[4],
+                      num_episodes = show[5],
+                      esrb         = show[6],
+                      description  = show[9],
+                      image        = show[2],
+                      url          = show[0],
+                      studio       = show[7])
+        studio = Studio(name       = show[7])
+        B = show[8].split(',')
+        for gn in B:
+            genres = AnimeGenres(anime_name = show[1], genre = gn)
+            db.session.add(genres)
+        #print(anime)
+        db.session.add(anime)
+        db.session.add(studio)
+    
+    db.session.commit()
--- fill_db_with_waifus.py
+++ fill_db_with_waifus.py
@@ -1,29 +1,29 @@
-from sys import stdin
 from app import db
 from app.models import Waifu, WaifuTags, WaifuAverages, Anime
 
-A = [line.split('$') for line in stdin]
-
-for i, character in enumerate(A):
-    waifu = Waifu(name        = character[1],
-                  description = character[5],
-                  image       = character[2],
-                  url         = character[0],
-                  anime_name  = Anime.query.filter_by(name=character[3]).first_or_404().name,
-                  anime_id    = Anime.query.filter_by(name=character[3]).first_or_404().id)
-    #print(waifu)
-    db.session.add(waifu)
-    B = character[4].split(',')
-    for ts in B:
-        tags = WaifuTags(waifu_name = character[1], tag = ts)
-        db.session.add(tags)
-
-    averages = WaifuAverages(waifu_id = str(i+1),
-                             appearance_total='0',
-                             personality_total='0',
-                             strength_total='0',
-                             intelligence_total='0',
-                             num_ratings='0')
-    db.session.add(averages)
-
-db.session.commit()
+with open('waifus.txt') as f:
+    A = [line.split('$') for line in f]
+    
+    for i, character in enumerate(A):
+        waifu = Waifu(name        = character[1],
+                      description = character[5],
+                      image       = character[2],
+                      url         = character[0],
+                      anime_name  = Anime.query.filter_by(name=character[3]).first_or_404().name,
+                      anime_id    = Anime.query.filter_by(name=character[3]).first_or_404().id)
+        #print(waifu)
+        db.session.add(waifu)
+        B = character[4].split(',')
+        for ts in B:
+            tags = WaifuTags(waifu_name = character[1], tag = ts)
+            db.session.add(tags)
+    
+        averages = WaifuAverages(waifu_id = str(i+1),
+                                 appearance_total='0',
+                                 personality_total='0',
+                                 strength_total='0',
+                                 intelligence_total='0',
+                                 num_ratings='0')
+        db.session.add(averages)
+    
+    db.session.commit()
